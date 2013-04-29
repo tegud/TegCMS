e2adf2e632e7c88361baec4d410adf95ad418a49
Steve Elliott <steve@tegud.net>
2013-04-29 07:07:27 +0100

    ViewModel test is red

--- TegCMS.Pages/PageModel.cs
+++ TegCMS.Pages/PageModel.cs
@@ -3,5 +3,7 @@ namespace TegCMS.Pages
     public class PageModel
     {
         public string ViewName { get; set; }
+
+        public object ViewModel { get; set; }
     }
 }
--- TegCMS.Tests/Controllers/PageControllerTests.cs
+++ TegCMS.Tests/Controllers/PageControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using NUnit.Framework;
 using TegCMS.Controllers;
 using TegCMS.Pages;
@@ -9,6 +10,7 @@ namespace TegCMS.Tests.Controllers
     public class PageControllerTests : IPageModelFactory
     {
         private string _expectedViewName = "expectedView";
+        private object _expectedViewModel;
 
         [Test]
         public void IndexReturnsSpecifiedLayout()
@@ -20,11 +22,22 @@ namespace TegCMS.Tests.Controllers
             Assert.That(result.ViewName, Is.EqualTo(_expectedViewName));
         }
 
+        [Test]
+        public void IndexReturnsSpecifiedViewModel()
+        {
+            _expectedViewModel = new Object();
+
+            var result = new PageController(this).Index();
+
+            Assert.That(result.ViewData.Model, Is.EqualTo(_expectedViewModel));
+        }
+
         public PageModel Build()
         {
-            return new PageModel()
+            return new PageModel
                 {
-                    ViewName = _expectedViewName
+                    ViewName = _expectedViewName,
+                    ViewModel = _expectedViewModel
                 };
         }
     }
